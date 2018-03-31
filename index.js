b6ab4dc8de75039a725fd99008f076fd63296ae7
discoduckling <mitalee.n.desai@gmail.com>
2018-03-31 09:45:55 -0600

    Refactor server file into routes and services folders

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
+
 
 const app = express();
 
-passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    }, (accessToken, refreshToken, profile, done) => {
-        console.log('access token', accessToken);
-        console.log('refresh token', refreshToken);
-        console.log('profile', profile);
-    })
-); // creates a new instance of google strategy
+require('./routes/authRoutes')(app); // authRoutes returns a function, then immediately call the function with the app object
 
-app.get('/auth/google', 
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
-    })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
