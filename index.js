f250b0e6ce7af83f4e177e5c8e06e35599d884cc
discoduckling <mitalee.n.desai@gmail.com>
2018-03-31 21:19:42 -0600

    Deploy with authentication

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
 
+mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000, // how long cookie can exist before it expires in ms
+        keys: [keys.cookieKey] // to sign or encrypt the cookie
+    })
+);
+app.use(passport.initialize()); // tells passport to use cookies
+app.use(passport.session());
 
 require('./routes/authRoutes')(app); // authRoutes returns a function, then immediately call the function with the app object
 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
-// module.exports = {
-//     googleClientID: '347502646278-4gf0i2040bkd3eq12sitsvh804jjh5kv.apps.googleusercontent.com',
-//     googleClientSecret: '52S4YiMPz62E3QMw1jYH-7Hy'
-// }
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,20 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users'); // don't need to import User, just call it from mongoose
+
+passport.serializeUser((user, done) => {
+    done(null, user.id); // .id is a short cut to _id in mongodb
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        })
+});
 
 passport.use(
     new GoogleStrategy({
@@ -8,9 +22,17 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
-        console.log('access token', accessToken);
-        console.log('refresh token', refreshToken);
-        console.log('profile', profile);
+        User.findOne({ googleId: profile.id })
+            .then(existingUser => {
+                if (existingUser) {
+                    // we already have a record with the given profile id
+                    done(null, existingUser); // first argument is null, normally it would pass back an error but we no there will be no error here
+                } else {
+                    new User({ googleId: profile.id })
+                        .save()
+                        .then(user => done(null, user));
+                }
+            })
     })
 ); // creates a new instance of google strategy
 
